fe3159f225c256f0af600696c9ddfde928adf727
turkialeid2030 <turkialeid@gmail.com>
2026-08-31 12:10:06 +0300

    valuation: export v1 intelligence framework

---
+++ src/valuation-intelligence/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  ...require('./contracts'),
+  ...require('./market-comparables'),
+  ...require('./income-capitalization'),
+  ...require('./cost-approach'),
+  ...require('./residual-approach'),
+  ...require('./reconciliation'),
+  ...require('./planner'),
+};
