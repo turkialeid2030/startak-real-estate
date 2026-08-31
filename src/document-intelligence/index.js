19543d2e9b808e072b8fb3dd61dacbeede2ce930
turkialeid2030 <turkialeid@gmail.com>
2026-08-31 10:45:15 +0300

    document-intelligence: expose semantic evidence mapping

--- src/document-intelligence/index.js
+++ src/document-intelligence/index.js
@@ -5,5 +5,6 @@ const pipeline = require('./pipeline');
 const reconciliation = require('./reconciliation');
 const readiness = require('./readiness');
 const parsers = require('./parsers');
+const semantics = require('./semantics');
 
-module.exports = Object.assign({}, contracts, pipeline, reconciliation, readiness, parsers, { parsers });
+module.exports = Object.assign({}, contracts, pipeline, reconciliation, readiness, parsers, semantics, { parsers, semantics });
