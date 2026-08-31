df80d7baf89744dcd573fe0312e91d6d94e35e63
turkialeid2030 <turkialeid@gmail.com>
2026-08-31 23:20:58 +0300

    Export decision-control orchestrator

--- src/project-model/index.js
+++ src/project-model/index.js
@@ -5,5 +5,6 @@ const evidencePlan = require('./evidence-plan');
 const engineRouter = require('./engine-router');
 const semanticCoverage = require('./semantic-coverage');
 const orchestrator = require('./universal-evidence-orchestrator');
+const decisionControl = require('./decision-control-orchestrator');
 
-module.exports = Object.assign({}, profile, evidencePlan, engineRouter, semanticCoverage, orchestrator);
+module.exports = Object.assign({}, profile, evidencePlan, engineRouter, semanticCoverage, orchestrator, decisionControl);
