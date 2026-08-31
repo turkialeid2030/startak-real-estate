6765043c1f38b980755680ac106123405445c20a
turkialeid2030 <turkialeid@gmail.com>
2026-08-31 10:10:31 +0300

    feat(document-intel): expose parser adapter layer

--- src/document-intelligence/index.js
+++ src/document-intelligence/index.js
@@ -4,5 +4,6 @@ const contracts = require('./contracts');
 const pipeline = require('./pipeline');
 const reconciliation = require('./reconciliation');
 const readiness = require('./readiness');
+const parsers = require('./parsers');
 
-module.exports = Object.assign({}, contracts, pipeline, reconciliation, readiness);
+module.exports = Object.assign({}, contracts, pipeline, reconciliation, readiness, parsers, { parsers });
