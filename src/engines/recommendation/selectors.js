00ef3057f11414c1f99176ea4d80f9a82d66d49a
turkialeid2030 <turkialeid@gmail.com>
2026-09-01 17:27:11 +0300

    feat(financial): expose hard-gate decision evidence

--- src/engines/recommendation/selectors.js
+++ src/engines/recommendation/selectors.js
@@ -1,10 +1,23 @@
-// src/engines/recommendation/selectors.js -- SELECTOR FACADE for recommendation/criteria view.
+'use strict';
+
 function selectRecommendationResult(engineResult) {
   return {
-    c1: engineResult.c1, c2: engineResult.c2, c3: engineResult.c3, c4: engineResult.c4, c5: engineResult.c5,
+    c0: engineResult.c0,
+    c1: engineResult.c1,
+    c2: engineResult.c2,
+    c3: engineResult.c3,
+    c4: engineResult.c4,
+    c5: engineResult.c5,
+    c6: engineResult.c6,
+    c7: engineResult.c7,
     metCount: engineResult.metCount,
     totalCriteria: engineResult.totalCriteria,
     verdict: engineResult.verdict,
+    decisionStatus: engineResult.decisionStatus,
+    criteriaDetail: engineResult.criteriaDetail,
+    failedHardGates: engineResult.failedHardGates,
+    failedSoftCriteria: engineResult.failedSoftCriteria,
+    financialModelStatus: engineResult.financialModelStatus,
   };
 }
 module.exports = { selectRecommendationResult };
