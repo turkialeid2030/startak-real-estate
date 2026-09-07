ac0ab94b1f5ee427ce69462923d2929f51349eeb
turkialeid2030 <turkialeid@gmail.com>
2026-09-07 13:28:56 +0300

    Wave 9C: add governed lease and income evidence

--- src/market/index.js
+++ src/market/index.js
@@ -2,8 +2,10 @@
 
 const comparableEvidence = require('./comparable-evidence');
 const comparableAdjustments = require('./comparable-adjustments');
+const leaseIncomeEvidence = require('./lease-income-evidence');
 
-module.exports = Object.assign({}, comparableEvidence, comparableAdjustments, {
+module.exports = Object.assign({}, comparableEvidence, comparableAdjustments, leaseIncomeEvidence, {
   comparableEvidence,
   comparableAdjustments,
+  leaseIncomeEvidence,
 });
