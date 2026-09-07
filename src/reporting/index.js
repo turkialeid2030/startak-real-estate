c19a3fb7eb23352a385003e73b8e15be0b44a845
turkialeid2030 <turkialeid@gmail.com>
2026-09-08 00:39:16 +0300

    Wave 15C: add report conformance QA and section traceability

--- src/reporting/index.js
+++ src/reporting/index.js
@@ -4,4 +4,5 @@ module.exports = {
   ...require('./financial-reporting-disclosure-packet'),
   ...require('./professional-report-contract'),
   ...require('./professional-review'),
+  ...require('./report-conformance-qa'),
 };
