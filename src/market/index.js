60f8d1fb62b67cd7a8933cc74d60859957a46017
turkialeid2030 <turkialeid@gmail.com>
2026-09-07 13:23:17 +0300

    Wave 9B: add professional comparable adjustment traceability

--- src/market/index.js
+++ src/market/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const comparableEvidence = require('./comparable-evidence');
+const comparableAdjustments = require('./comparable-adjustments');
 
-module.exports = Object.assign({}, comparableEvidence, {
+module.exports = Object.assign({}, comparableEvidence, comparableAdjustments, {
   comparableEvidence,
+  comparableAdjustments,
 });
